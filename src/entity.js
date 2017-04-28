5f30a4519318b80c0d4eaacb319ff7ab15b2602b
Fora <luoxuemai@boqii.com>
2017-04-28 13:46:20 +0800

    ES6 Entity

--- src/entity.js
+++ src/entity.js
@@ -1,279 +1,341 @@
-/*!
- * Expose `Entity`.
- */
-module.exports = Entity;
+import Immutable from 'seamless-immutable';
 
-/*!
- * Module dependencies.
- */
 var util = require('util');
-var debug = require('debug')('poplar:entity');
-var inherits = util.inherits;
+var debug = require('debug')('boqii-plant:entity');
 var _ = require('lodash');
 var assert = require('assert');
-
 var Dynamic = require('./dynamic');
 
 /**
  * @class A wrapper to map returns with input value.
  */
-function Entity(definitions) {
-  this._mappings = {};
-  this._keys = [];
-
-  var self = this;
-
-  if (_.isPlainObject(definitions)) {
-    _.each(definitions, function(fn, name) {
-      if (fn === true) {
-        self.add(name);
-      } else if (Array.isArray(fn)){
-        fn.unshift(name);
-        self.add.apply(self, fn);
-      } else {
-        fn = [name, fn];
-        self.add.apply(self, fn);
-      }
-    });
-  } else if (!_.isUndefined(definitions)) {
-    assert(false, util.format('\'%s\' is not a valid object', definitions));
+export default class Entity {
+  constructor(name, definitions) {
+    this._name = name || 'Entity';
+    this._mappings = {};
+    this._excepts = false;
+    this.define(definitions);
   }
-}
 
-/**
- * check whether an object is an Entity instance
- */
-Entity.isEntity = function(entity) {
-  return !!(entity && _.isFunction(entity.isEntity) && this.prototype.isEntity.call(entity));
-};
-
-/**
- * add a given name with or without corresponding function or value
- * { act: 'alias',
- *   value: 'name',
- *   default: null,
- *   using: MyEntity,
- *   if: function(obj, opts) {}
- * }
- * type: support array type
- *    number or ['number']
- *    date or ['date']
- *    string or ['string']
- *    boolean or ['boolean']
- *    any (default) or ['any']
- * act:
- *    function
- *    alias
- *    value
- * Usage:
- *    var entity = new Entity();
- *    entity.add('name');
- *    entity.add('name', { as: 'fullname' });
- *    entity.add('name', { type: 'string', as: 'fullname' });
- *    entity.add('sex', { value: 'male' });
- *    entity.add('isAdult', function(obj) { return obj && obj.age >= 18; });
- *    entity.add('activities', { using: myActivityEntity });
- *    entity.add('extraInfo', { using: myExtraInfoEntity });
- *    entity.add('condition', { if: function(obj, options) { return true } });
- */
-Entity.prototype.add = function() {
-  // ...names, options, fn
-
-  var message = '\'%s\' is not a valid string';
-  var self = this;
-
-  assert(arguments.length !== 0, util.format(message, undefined));
-
-  var options, fn;
-
-  if (arguments.length > 1) {
-
-    // extract `fn`
-    if (_.isFunction(_.last(arguments))) {
-      fn = Array.prototype.pop.call(arguments);
+  extend(name) {
+    if (Immutable.isImmutable(this)) {
+      const mutable = this.asMutable({ deep: true });
+      mutable._name = name;
+      return mutable;
+    } else {
+      throw new Error('Cannot extend an mutable entity: ' + this._name);
     }
+  }
 
-    // extract `options`
-    options = _.isPlainObject(_.last(arguments)) ? Array.prototype.pop.call(arguments) : {};
+  asImmutable() {
+    return Immutable(this, { prototype: Entity.prototype });
+  }
+
+  define(definitions) {
+    var self = this;
+
+    if (_.isPlainObject(definitions)) {
+      _.each(definitions, function(fn, name) {
+        if (fn === true) {
+          self.expose(name);
+        } else if (Array.isArray(fn)) {
+          fn.unshift(name);
+          self.expose.apply(self, fn);
+        } else {
+          fn = [name, fn];
+          self.expose.apply(self, fn);
+        }
+      });
+    } else if (!_.isUndefined(definitions)) {
+      assert(false, util.format('\'%s\' is not a valid object', definitions));
+    }
+  }
+
+  /**
+   * check whether an object is an Entity instance
+   */
+  static isEntity(entity) {
+    return !!(entity && _.isFunction(entity.isEntity) && this.prototype.isEntity.call(entity));
+  }
+
+
+  /**
+   * Include all fields except
+   */
+  excepts() {
+    let excepts = this._excepts || [];
+    this._excepts = _.union(excepts, _.values(arguments), ['__v']);
+  }
+
+  /**
+   * add a given name with or without corresponding function or value
+   * { act: 'alias',
+   *   value: 'name',
+   *   default: null,
+   *   using: MyEntity,
+   *   if: function(obj, opts) {}
+   * }
+   * type: support array type
+   *    number or ['number']
+   *    date or ['date']
+   *    string or ['string']
+   *    boolean or ['boolean']
+   *    any (default) or ['any']
+   * act:
+   *    function
+   *    alias
+   *    value
+   * Usage:
+   *    var entity = new Entity();
+   *    entity.expose('name');
+   *    entity.expose('name', { as: 'fullname' });
+   *    entity.expose('fullname', { get: 'name' });
+   *    entity.expose('name', { type: 'string', as: 'fullname' });
+   *    entity.expose('sex', { value: 'male' });
+   *    entity.expose('isAdult', function(obj) { return obj && obj.age >= 18; });
+   *    entity.expose('activities', { using: myActivityEntity });
+   *    entity.expose('extraInfo', { using: myExtraInfoEntity });
+   *    entity.expose('condition', { if: function(obj, options) { return true } });
+   */
+  expose() {
+    // ...names, options, fn
+
+    var message = '\'%s\' is not a valid string';
+    var self = this;
+
+    assert(arguments.length !== 0, util.format(message, undefined));
+
+    var options, fn;
 
     if (arguments.length > 1) {
-      assert(!options.as, 'You may not use the :as option on multi-attribute exposures.');
-      assert(!fn, 'You may not use function on multi-attribute exposures.');
+
+      // extract `fn`
+      if (_.isFunction(_.last(arguments))) {
+        fn = Array.prototype.pop.call(arguments);
+      }
+
+      // extract `options`
+      options = _.isPlainObject(_.last(arguments)) ? Array.prototype.pop.call(arguments) : {};
+
+      if (arguments.length > 1) {
+        assert(!options.as, 'You may not use the :as option on multi-attribute exposures.');
+        assert(!fn, 'You may not use function on multi-attribute exposures.');
+      }
+
+    } else {
+      options = {};
     }
 
-  } else {
-    options = {};
-  }
-
-  _.each(arguments, function(name) {
-    var value = null,
+    _.each(arguments, function(name) {
+      var value = null,
         defaultVal = null,
         act = null,
         type = null,
         using = null,
         ifFn = null;
+      var validName = (_.isString(name)) && (/^[a-zA-Z0-9_\.]+$/g.test(name));
+      var invalidAsFunc = (options.as && fn);
+      var invalidValueFunc = (options.as && fn);
+      var invalidValueAs = (options.as && fn);
+      assert(validName, util.format(message, name)); // name must be a string
+      assert(!invalidAsFunc, 'You can not use the :as option with function.');
+      assert(!invalidValueFunc, 'You can not use the :value option with function.');
+      assert(!invalidValueAs, 'You can not use the :value option with :as option.');
 
-    assert(_.isString(name) && /^[a-zA-Z0-9_]+$/g.test(name), util.format(message, name)); // name must be a string
-    assert(!(options.as && fn), 'You can not use the :as option with function.');
-    assert(!(options.value && fn), 'You can not use the :value option with function.');
-    assert(!(options.value && options.as), 'You can not use the :value option with :as option.');
+      if (Array.isArray(options.type)) {
+        type = [options.type[0] || 'any'];
+      } else {
+        type = options.type || 'any';
+      }
+      type = type.toLowerCase();
 
-    if (Array.isArray(options.type)) {
-      type = [options.type[0] || 'any'];
-    } else {
-      type = options.type || 'any';
-    }
+      act = 'alias';
+      value = name;
 
-    act = 'alias';
-    value = name;
-
-    if (fn) {
-      act = 'function';
-      value = fn;
-    }
-
-    if (!_.isEmpty(options)) {
-
-      if (options.hasOwnProperty('default')) {
-        defaultVal = _.isUndefined(options['default']) ? null : options['default'];
+      if (fn) {
+        act = 'function';
+        value = fn;
       }
 
-      if (options['if']) {
-        assert(_.isFunction(options['if']), 'if condition must be a function');
-        ifFn = options['if'];
+      if (!_.isEmpty(options)) {
+
+        if (options.hasOwnProperty('default')) {
+          defaultVal = _.isUndefined(options['default']) ? null : options['default'];
+        }
+
+        if (options['if']) {
+          assert(_.isFunction(options['if']), 'if condition must be a function');
+          ifFn = options['if'];
+        }
+
+        if (options.using) {
+          assert(Entity.isEntity(options.using), self._name + ' `using` ' + options.using._name + ' must be a Entity');
+          using = options.using;
+        }
+
+        if (options.as) {
+          if (!_.isString(options.as)) throw (new Error(util.format(message, options.as)));
+          act = 'alias';
+          value = name;
+          name = options.as;
+        } else
+        if (options.get) {
+          act = 'get';
+          value = options.get;
+        } else
+        if (options.value) {
+          act = 'value';
+          value = options.value;
+        }
+
       }
-
-      if (options.using) {
-        assert(Entity.isEntity(options.using), '`using` must be a Entity');
-        using = options.using;
-      }
-
-      if (options.as) {
-        if (!_.isString(options.as)) throw(new Error(util.format(message, options.as)));
-        act = 'alias';
-        value = name;
-        name = options.as;
-      } else if (options.value) {
-        act = 'value';
-        value = options.value;
-      }
-
-    }
-
-    self._mappings[name] = {
-      type: type,
-      act: act,
-      value: value,
-      default: defaultVal,
-      if: ifFn,
-      using: using
-    };
-
-    // always get all keys after add a new field
-    self._keys = Object.keys(self._mappings);
-  });
-};
-
-/**
- * Entity.prototype.add alias
- */
-Entity.prototype.expose = Entity.prototype.add;
-
-/**
- * parse a input object with mappings
- * @param {Object} input: input object values
- * @param {Function} converter: value converter, which can accept one parameter
- */
-Entity.prototype.parse = function(input, options, converter) {
-  debug('parsing %j with options %j and converter', input, options);
-  var originalObj;
-  var result = {};
-  var self = this;
-
-  if (_.isUndefined(input) || _.isNull(input)) {
-    originalObj = {};
-  } else {
-    originalObj = input;
-  }
-
-  if (_.isFunction(options)) {
-    converter = options;
-  }
-
-  if (!_.isPlainObject(options)) {
-    options = {};
-  }
-
-  if (Array.isArray(originalObj)) {
-    // if input is an Array, then loop it
-    result = [];
-    _.each(originalObj, function(obj) {
-      result.push(self.parse(obj, options, converter));
+      //debug(self._name, 'expose', name);
+      self._mappings[name] = {
+        type: type,
+        act: act,
+        value: value,
+        default: defaultVal,
+        if: ifFn,
+        using: using
+      };
     });
-    return result;
+  }
 
-  } else {
-    if (_.isEmpty(self._keys)) {
-      // if no exposes, return
-      return result;
+
+  /**
+   * parse a input object with mappings
+   * @param {Object} input: input object values
+   * @param {Function} converter: value converter, which can accept one parameter
+   */
+  parse(input, options, converter) {
+    var originalObj;
+    var result = {};
+    var self = this;
+
+    if (_.isUndefined(input) || _.isNull(input)) {
+      originalObj = {};
     } else {
-      _.each(self._keys, function(k) {
-        var o = self._mappings[k];
-        var val = null;
+      if (typeof input.toObject === 'function') { // mongoose object
+        originalObj = input.toObject();
+      } else
+      if (typeof input.toJSON === 'function') {   // sequelize object
+        originalObj = input.toJSON();
+      } else {
+        originalObj = input;
+      }
+    }
 
-        if (o['if'] && !o['if'](originalObj, options)) {
-          return;
-        }
+    if (_.isFunction(options)) {
+      converter = options;
+    }
 
-        switch(o.act){
-          case 'function':
-            try {
-              val = o.value(originalObj, options);
-            } catch(e) {
-              console.error(e);
-              val = null;
-            }
-            break;
-          case 'alias':
-            val = originalObj[o.value];
-            break;
-          case 'value':
-            val = o.value;
-            break;
-        }
+    if (!_.isPlainObject(options)) {
+      options = {};
+    }
 
-        var isDefaultValueApplied = false;
-        // if value is `null`, `undefined`, set default value
-        if (_.isUndefined(val) || _.isNull(val)) {
-          val = o.default;
-          isDefaultValueApplied = true;
-        }
-
-        if (converter && _.isFunction(converter)) {
-          val = converter(val, options);
-        }
-
-        if (!isDefaultValueApplied && o.using) {
-          val = o.using.parse(val, options, converter);
-        }
-
-        // cast type according to predefined dynamic converters
-        try {
-          val = Dynamic.convert(val, o.type, options);
-        } catch (e) {
-          console.error(e);
-        }
-
-        result[k] = val;
+    //debug(this._name, 'parsing object', originalObj);
+    if (Array.isArray(originalObj)) {
+      // if input is an Array, then loop it
+      result = [];
+      _.each(originalObj, function(obj) {
+        result.push(self.parse(obj, options, converter));
       });
       return result;
+    } else {
+      let keys = Object.keys(self._mappings);
+      if (_.isEmpty(keys)) {
+        // if no exposes, return
+        return result;
+      } else
+      if (!_.isObject(originalObj)) {
+        return originalObj;
+      } else {
+        // if excepts enabled then use all keys
+        if (self._excepts) {
+          keys = _.union(keys, _.keys(originalObj));
+          keys = _.difference(keys, self._excepts);
+        }
+        // sort keys and put id at the first
+        keys = _.sortBy(keys);
+        if (_.pull(keys, 'id')) {
+          keys.unshift('id');
+        }
+        //debug(self._name, 'mappings', keys, 'excepts', self._excepts);
+        _.each(keys, function(k) {
+          var o = self._mappings[k] || { act: 'alias', value: k, type: 'any' };
+          var val = null;
+
+          //debug(self._name, 'map ', k, 'o', o);
+          if (o['if'] && !o['if'](originalObj, options)) {
+            return;
+          }
+
+          switch (o.act) {
+            case 'function':
+              try {
+                //debug('entity value function', o.value);
+                val = o.value(originalObj, options);
+              } catch (e) {
+                console.error('entity function error', o.value, e);
+                val = null;
+              }
+              break;
+            case 'alias':
+              val = _.get(originalObj, o.value);
+              break;
+            case 'get':
+              val = _.get(originalObj, o.value);
+              if (_.isUndefined(val)) {
+                debug('missing get %s %s of %s', self._name, o.value, originalObj._id);
+              }
+              break;
+            case 'value':
+              val = o.value;
+              break;
+          }
+
+          var isDefaultValueApplied = false;
+          // if value is `null`, `undefined`, set default value
+          if (_.isUndefined(val) || _.isNull(val)) {
+            val = o.default;
+            isDefaultValueApplied = true;
+          }
+
+          if (converter && _.isFunction(converter)) {
+            val = converter(val, options);
+          }
+
+          //debug(self._name, 'using ', isDefaultValueApplied, o.using);
+          if (!isDefaultValueApplied && o.using) {
+            //debug(self._name, 'using entity', val, o.using);
+            if (!_.isFunction(o.using.parse)) {
+              debug('#### Invalid Entity Using ####\n', self._name, 'using entity', val, o.using._name);
+            }
+            val = o.using.parse(val, options, converter);
+          }
+
+          // cast type according to predefined dynamic converters
+          try {
+            val = Dynamic.convert(val, o.type, options);
+          } catch (e) {
+            console.error("entity dynamic convert error", e);
+          }
+
+          _.set(result, k, val);
+        });
+        // for convenience
+        if (!keys.id && !result.id) {
+          result.id = originalObj._id || originalObj.id;
+        }
+        return result;
+      }
     }
   }
-};
 
-/**
- * check if an object is an Entity
- */
-Entity.prototype.isEntity = function() {
-  return this instanceof Entity;
-};
+  /**
+   * check if an object is an Entity
+   */
+  isEntity() {
+    return this instanceof Entity;
+  }
+}
