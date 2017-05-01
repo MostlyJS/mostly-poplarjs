6b84e4153c44c8d538ceaa242b7fe2348fd7118e
Fora <luoxuemai@boqii.com>
2017-05-02 03:17:06 +0800

    Babel env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+if (!global._babelPolyfill) { require('babel-polyfill'); }
+
 module.exports = require('./lib/poplar');
 module.exports.ApiBuilder = require('./lib/api_builder');
 module.exports.ApiMethod = require('./lib/api_method');
