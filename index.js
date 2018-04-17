0569da3d876b52a089ffc5e478b284f83ea1767f
forafish <fora.luo@gmail.com>
2018-04-17 19:20:05 +0800

    add esm, remove babel

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-if (!global._babelPolyfill) { require('babel-polyfill'); }
-
-module.exports = require('./lib/poplar');
-module.exports.ApiBuilder = require('./lib/api_builder');
-module.exports.ApiMethod = require('./lib/api_method');
-module.exports.Validate = require('./lib/validation');
-module.exports.Sanitize = require('./lib/sanitizer');
+require = require("esm")(module/*, options*/);
+console.time('mostly-poplarjs import');
+module.exports = require('./src/poplar').default;
+module.exports.ApiBuilder = require('./src/api_builder').default;
+module.exports.ApiMethod = require('./src/api_method').default;
+module.exports.Validate = require('./src/validation').default;
+module.exports.Sanitize = require('./src/sanitizer').default;
+console.timeEnd('mostly-poplarjs import');
